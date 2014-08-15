b9f58ff7a361b19653353e011e827d7a3de98b80
Levi Linville <Levi.Linville@inin.com>
2014-08-15 13:17:18 -0400

    Added files from local. Backup.

--- src/com/pathfinder/Pathfinder.java
+++ src/com/pathfinder/Pathfinder.java
@@ -11,6 +11,7 @@ public class Pathfinder {
     private Direction[][] directions;
     private boolean[][] solved;
 
+    private Direction[][] inverseDirections = {{SOUTHEAST, SOUTH, SOUTHWEST},{EAST, NONE, WEST,},{NORTHEAST, NORTH, NORTHWEST}};
     public Pathfinder(int width, int height){
         MAPWIDTH = width;
         MAPHEIGHT = height;
@@ -53,23 +54,33 @@ public class Pathfinder {
         }
     }
 
+    private void updatePointInQueue(PriorityQueue<Point> queue, Point toUpdate, double newWeight){
+        queue.remove(toUpdate);
+        toUpdate.distance = newWeight;
+        queue.add(toUpdate);
+    }
+
     public void updateNeighbors(PriorityQueue<Point> queue, Point p){
         Point dummyPointToRemove = new Point(0,0,0);
         int px = p.x;
         int py = p.y;
         double cWeight = weights[px][py]; //weight of currentPoint
+        double alt;
 
         for(int xOffset=-1; xOffset<=1; xOffset++){
             for(int yOffset=-1; yOffset<=1; yOffset++){
-                if(distances[px][py]+cWeight < distances[px+xOffset][py+yOffset]){
-                    distances[px+xOffset][py+yOffset] = distances[px][py]+cWeight;
-                    directions[px+xOffset][py+yOffset] = SOUTHEAST;
+                alt = distances[px][py]+cWeight;
+                if(px>1 && py> 1 &&px<MAPWIDTH-2 && py<MAPHEIGHT-2 && !solved[px+xOffset][py+yOffset]) {
+                    queue.add(new Point(px + xOffset, py + yOffset, Double.POSITIVE_INFINITY));
+                }
+                if(alt < distances[px+xOffset][py+yOffset]){
+                    distances[px+xOffset][py+yOffset] = alt;
+                    directions[px+xOffset][py+yOffset] = inverseDirections[xOffset+1][yOffset+1];
                     dummyPointToRemove.x = px+xOffset;
                     dummyPointToRemove.y = py+yOffset;
                     dummyPointToRemove.distance = distances[px][py];
-                    queue.remove(dummyPointToRemove);
-                    dummyPointToRemove.distance = distances[px][py]+cWeight;
-                    queue.add(dummyPointToRemove);
+                    updatePointInQueue(queue, dummyPointToRemove, alt);
+
                 }
             }
         }
@@ -86,7 +97,7 @@ public class Pathfinder {
                 solved[i][j]=false;
                 distances[i][j]=Double.POSITIVE_INFINITY;
                 directions[i][j]=NONE;
-                toSearch.add( new Point(i,j,Double.POSITIVE_INFINITY) );
+                //toSearch.add( new Point(i,j,Double.POSITIVE_INFINITY) );
             }
         }
         distances[xTarget][yTarget]=0;
@@ -96,13 +107,13 @@ public class Pathfinder {
         while(!toSearch.isEmpty()){
             //printDistances();
             Point currentPoint = toSearch.poll();
+            solved[currentPoint.x][currentPoint.y] = true;
 
             /*
             for(Point p : toSearch.toArray(new Point[toSearch.size()])){
                 System.out.print("/t("+p.x+","+p.y+")");
             } System.out.println("\n");
             */
-
             updateNeighbors(toSearch, currentPoint);
         }
 
--- src/com/pathfinder/PathfinderDemo.java
+++ src/com/pathfinder/PathfinderDemo.java
@@ -2,18 +2,33 @@ package com.pathfinder;
 
 public class PathfinderDemo {
     public static void main(String[] args){
-        for(int size=5; size<500; size+=10) {
+        try{
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        while(true)
+        for(int size=10; size<300; size=size+size/4) {
 
             Pathfinder pathfinder = new Pathfinder(size,size);
             long startTime = System.nanoTime();
-            pathfinder.calculateDistances(1, 1);
+            pathfinder.calculateDistances(5, 5);
             long endTime = System.nanoTime();
 
             long duration = endTime - startTime;
             System.out.println("Size: "+size+"\tTime: "+duration/1000000+"ms");
 
         }
-        //pathfinder.printDistances();
+        /*
+        Pathfinder pathfinder = new Pathfinder(30,30);
+        long startTime = System.nanoTime();
+        pathfinder.calculateDistances(5, 5);
+        long endTime = System.nanoTime();
 
+        long duration = endTime - startTime;
+        System.out.println("Size: "+30+"\tTime: "+duration/1000000+"ms");
+        pathfinder.printDistances();
+*/
     }
 }
